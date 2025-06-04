071882b937798b27ae33be3bc1dd03b54542a6fd
Minh Kha <khatruong@Khas-MacBook-Air-2.local>
2025-06-04 15:00:06 +1000

    Adding transformation for Navbar

--- portfolio/src/components/navbar.tsx
+++ portfolio/src/components/navbar.tsx
@@ -13,16 +13,22 @@ const Navbar: React.FC<NavbarProps> = ({ onNavClick }) => {
     ];
 
     return (
-        <nav className="fixed top-0 left-0 w-full bg-nav-cl text-white font-montserrat-bold flex items-center justify-between px-8 py-4 z-30">
-            <div className="flex items-center space-x-3">
+        <nav className="fixed top-0 left-0 w-full xl:px-8 py-4 z-30 bg-nav-cl
+                         text-white font-montserrat-bold 
+                         flex items-center justify-between ">
+            {/* Area for home button */}
+            <div className="flex items-center space-x-3 px-4 cursor-pointer">
                 <MdHomeFilled className="h-6 w-6" />
                 <span className="text-xl">Marvin</span>
             </div>
-            <ul className="flex space-x-8 text-lg">
+
+            {/* Area for button */}
+            <ul className="flex space-x-6 text-xl">
                 {navItems.map(({ id, label }) => (
                     <li key={id}>
                         <button
                             onClick={() => onNavClick(id)}
+                            className="hover:text-nav-cl hover:bg-white hover: rounded-2xl px-4 py-2 transition-colors duration-300 cursor-pointer"
                         >
                             {label}
                         </button>
